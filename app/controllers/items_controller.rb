50f0f8c98743a565173d1964f4f283370f05f9a2
hero-39 <high.quality.of.life@gmail.com>
2023-09-01 15:56:58 +0900

    devise導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,12 @@ class ItemsController < ApplicationController
   def index  # indexアクションを定義した
     # @items = Item.all
   end
- 
+
+  def new  # indexアクションを定義した
+   # @items= Items.new
+  end
+
+  def create  # createアクションを定義した
+  end
+
  end
