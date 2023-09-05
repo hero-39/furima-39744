f14246293f9e6cb0c5bde76346bf3b98d9f00f0d
hero-39 <high.quality.of.life@gmail.com>
2023-09-05 18:11:53 +0900

    itemモデルにバリデーションを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :image, presence: true
+  validates :product_name, presence: true
+  validates :description_of_item, presence: true
+  validates :price, presence: true
+
+  with_options presence: true, format: { with: /\A\d+\z/, message: '全角数字を使用してください' } do
+    validates :price
+ 
+    validates :price, presence: true, inclusion: { in: 300..9_999_999 }
+  
 end
